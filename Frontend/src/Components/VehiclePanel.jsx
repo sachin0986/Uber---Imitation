f9cb1103b48096b7b748e0b3920b382e1dce6c7f
Sachin Arora <sachinarora0986@gmail.com>
2025-06-29 16:43:51 +0530

    Confirm ride panel created with closing function

---
+++ Frontend/src/Components/VehiclePanel.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { RiUser3Fill } from "react-icons/ri";
+import { RiArrowDownWideFill } from "react-icons/ri";
+
+const VehiclePanel = (props) => {
+    return(
+        <div>
+               <div
+                  className="absolute top-2 right-5 cursor-pointer"
+                  onClick={() => props.setVehiclePanelOpen(false)}
+                >
+                  <RiArrowDownWideFill size={24} />
+                </div>
+                <div
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                }}
+                className="flex w-full p-3 active:border-2  border-black mb-3 rounded-2xl items-center justify-between">
+                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png" alt="" />
+                <div className="w-1/2">
+                    <h4 className="font-medium flex gap-2 text-base">Uber Go <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>4</span></h4>
+                    <h5 className="font-medium text-sm">2 mins away</h5>
+                    <p className="font-medium text-xs text-gray-600">Affordable, compact rides</p>
+                </div>
+                    <h2 className="text-lg font-semibold ">₹ 193.20</h2>
+                </div>
+                <div 
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                }}
+                className="flex w-full p-3 active:border-2 mb-3 border-black rounded-2xl items-center justify-between">
+                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1698944322/assets/92/00189a-71c0-4f6d-a9de-1b6a85239079/original/UberMoto-India-Orange.png" alt="" />
+                <div className="w-1/2">
+                    <h4 className="font-medium flex gap-2 text-base">Uber Moto <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>1</span></h4>
+                    <h5 className="font-medium text-sm">3 mins away</h5>
+                    <p className="font-medium text-xs text-gray-600">Affordable, MoterCycle rides</p>
+                </div>
+                    <h2 className="text-lg font-semibold ">₹ 65.00</h2>
+                </div>
+                <div 
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                }}
+                className="flex w-full p-3 active:border-2 mb-3 border-black rounded-2xl items-center justify-between">
+                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+                <div className="w-1/2">
+                    <h4 className="font-medium flex gap-2 text-base">Uber Auto <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>3</span></h4>
+                    <h5 className="font-medium text-sm">2 mins away</h5>
+                    <p className="font-medium text-xs text-gray-600">Affordable, auto rides</p>
+                </div>
+                    <h2 className="text-lg font-semibold ">₹ 118.24</h2>
+                </div>
+        </div>
+    )
+}
+
+export default VehiclePanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,8 @@ import { RiArrowDownWideFill } from "react-icons/ri";
 import { GrEmptyCircle } from "react-icons/gr";
 import { LuSquareSquare } from "react-icons/lu";
 import LocationSearchPanel from "../Components/LocationSearchPanel";
-import { RiUser3Fill } from "react-icons/ri";
+import VehiclePanel from "../Components/VehiclePanel";
+import ConfirmRide from "../Components/ConfirmRide";
 
 const style = {
   inputStyle: `bg-[#eee] px-12 py-2 text-base rounded-lg w-full`,
@@ -18,9 +19,11 @@ const Home = () => {
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
   const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false);
+  const [ConfirmRidePanel, setConfirmRidePanel] = useState(false);
 
   const panelCloseRef = useRef(null);
   const vehiclePanelRef = useRef(null);
+  const ConfirmRidePanelRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -63,6 +66,23 @@ else{
     })
 }
   }, [vehiclePanelOpen])
+
+
+
+  useGSAP(() => {
+    if(ConfirmRidePanel){
+        gsap.to(ConfirmRidePanelRef.current, {
+        transform: 'translateY(0)'
+    })
+}
+else{
+    gsap.to(ConfirmRidePanelRef.current, {
+        transform: 'translateY(100%)'
+    })
+}
+  }, [ConfirmRidePanel])
+
+
 
   return (
     <div className="h-screen relative overflow-hidden">
@@ -139,40 +159,21 @@ else{
         </div>
       </div>
       <div ref={vehiclePanelRef} className="fixed w-full z-10 bg-white translate-y-full bottom-0 px-3 py-6">
-                <div
-                  className="absolute top-2 right-5 cursor-pointer"
-                  onClick={() => setVehiclePanelOpen(false)}
-                >
-                  <RiArrowDownWideFill size={24} />
-                </div>
-                <div className="flex w-full p-3 active:border-2  border-black mb-3 rounded-2xl items-center justify-between">
-                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png" alt="" />
-                <div className="w-1/2">
-                    <h4 className="font-medium flex gap-2 text-base">Uber Go <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>4</span></h4>
-                    <h5 className="font-medium text-sm">2 mins away</h5>
-                    <p className="font-medium text-xs text-gray-600">Affordable, compact rides</p>
-                </div>
-                    <h2 className="text-lg font-semibold ">₹ 193.20</h2>
-                </div>
-                <div className="flex w-full p-3 active:border-2 mb-3 border-black rounded-2xl items-center justify-between">
-                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1698944322/assets/92/00189a-71c0-4f6d-a9de-1b6a85239079/original/UberMoto-India-Orange.png" alt="" />
-                <div className="w-1/2">
-                    <h4 className="font-medium flex gap-2 text-base">Uber Moto <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>1</span></h4>
-                    <h5 className="font-medium text-sm">3 mins away</h5>
-                    <p className="font-medium text-xs text-gray-600">Affordable, MoterCycle rides</p>
-                </div>
-                    <h2 className="text-lg font-semibold ">₹ 65.00</h2>
-                </div>
-                <div className="flex w-full p-3 active:border-2 mb-3 border-black rounded-2xl items-center justify-between">
-                <img className="h-16" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_768/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
-                <div className="w-1/2">
-                    <h4 className="font-medium flex gap-2 text-base">Uber Auto <span className="flex gap-1 items-center justify-center"><RiUser3Fill className=""/>3</span></h4>
-                    <h5 className="font-medium text-sm">2 mins away</h5>
-                    <p className="font-medium text-xs text-gray-600">Affordable, auto rides</p>
-                </div>
-                    <h2 className="text-lg font-semibold ">₹ 118.24</h2>
-                </div>
+                <VehiclePanel setConfirmRidePanel={setConfirmRidePanel} setVehiclePanelOpen={setVehiclePanelOpen}/>
             </div>
+
+        <div ref={ConfirmRidePanelRef} className="fixed w-full z-10 bg-white translate-y-full bottom-0 px-3 py-6">
+          <div className="relative">
+            <h5
+              onClick={() => setConfirmRidePanel(false)}
+              className="absolute right-4 top-2 cursor-pointer"
+            >
+              <RiArrowDownWideFill size={20} />
+            </h5>
+            <ConfirmRide setConfirmRidePanel={setConfirmRidePanel} />
+          </div>
+        </div>
+        
     </div>
   );
 };
