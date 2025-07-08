7cc1d61e09ba41bfb0e8578ff96438e294434163
Sachin Arora <sachinarora0986@gmail.com>
2025-07-09 03:56:26 +0530

    auto suggestions route completed

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -18,5 +18,11 @@ router.get('/get-distanse-time',
     mapController.getDistanceTime
 )
 
+router.get('/get-suggestions',
+    query('input').isString().isLength({ min: 3 }),
+    authMiddleware.authUser,
+    mapController.getAutoCompleteSuggestions
+)
+
 
 module.exports = router;
--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -52,3 +52,26 @@ module.exports.getDistanceTime = async (origin, destination) => {
         throw err;
     }
 }
+
+
+
+module.exports.getAutoCompleteSuggestions = async (input) => {
+    if (!input) {
+        throw new Error('query is required');
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            return response.data.predictions.map(prediction => prediction.description).filter(value => value);
+        } else {
+            throw new Error('Unable to fetch suggestions');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
